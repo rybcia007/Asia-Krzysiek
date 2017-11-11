8556dbe4b7d6844f40cbbb56c058d626d2a7c540
Krzysztof Rybka <fochokropcia@gmail.com>
2017-11-11 10:56:48 +0100

    0.0.0.0

--- api/config/database.php
+++ api/config/database.php
@@ -1,25 +1,20 @@
 <?php
-class Database {
-    private $host = "sql.krzysiekrybk.nazwa.pl";
-    private $db_name = "api_db";
-    private $username = "krzysiekrybk";
-    private $password = "Dorotka@123";
-    public $conn;
- 
-    public function getConnection(){
- 
-        $this->conn = null;
- 
-        try{
-            $this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->db_name, $this->username, $this->password);
-            $this->conn->exec("set names utf8");
-            echo "Connected successfully";
-        }catch(PDOException $exception){
-            echo "Connection error: " . $exception->getMessage();
-        }
- 
-        return $this->conn;
-    }
-}
+  class Database{
+      private $host = "sql.krzysiekrybk.nazwa.pl";
+      private $db_name = "krzysiekrybk";
+      private $username = "krzysiekrybk";
+      private $password = "Dorotka@123";
+      public $conn;
 
+      public function getConnection(){
+          $this->conn = null;
+          try{
+              $this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->db_name, $this->username, $this->password);
+              $this->conn->exec("set names utf8");
+          }catch(PDOException $exception){
+              echo "Connection error: " . $exception->getMessage();
+          }
+          return $this->conn;
+      }
+  }
 ?>
